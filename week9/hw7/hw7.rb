b047dcf8ef826e605e9f2f4a85e2782d44bf2db7
= <=>
2024-01-03 16:14:06 -0500

    add init methods for all the classes; implement Line class methods

--- week9/hw7/hw7.rb
+++ week9/hw7/hw7.rb
@@ -53,6 +53,14 @@ class GeometryValue
     end
   end
 
+  def check_for_no_points condition
+    if condition
+      self
+    else
+      NoPoints.new()
+    end
+  end
+
   public
   # we put this in this class so all subclasses can inherit it:
   # the intersection of self with a NoPoints is a NoPoints object
@@ -152,95 +160,181 @@ class Point < GeometryValue
   end
 
   private
-  def check_for_no_points condition
-    if condition
-      self
-    else
-      NoPoints.new()
-    end
-  end
-
   def inbetween(v, end1, end2)
     epsilon = GeometryExpression::Epsilon
     (end1 - epsilon <= v && v <= end2 + epsilon) || (end2 - epsilon <= v && v <= end1 + epsilon)
   end
 end
 
-# class Line < GeometryValue
-#   # *add* methods to this class -- do *not* change given code and do not
-#   # override any methods
-#   attr_reader :m, :b
-#   def initialize(m,b)
-#     @m = m
-#     @b = b
-#   end
-# end
+class Line < GeometryValue
+  # *add* methods to this class -- do *not* change given code and do not
+  # override any methods
+  attr_reader :m, :b
+  def initialize(m,b)
+    @m = m
+    @b = b
+  end
 
-# class VerticalLine < GeometryValue
-#   # *add* methods to this class -- do *not* change given code and do not
-#   # override any methods
-#   attr_reader :x
-#   def initialize x
-#     @x = x
-#   end
-# end
+  def shift(dx,dy)
+    Line.new(@m, @b + dy - @m * dx)
+  end
 
-# class LineSegment < GeometryValue
-#   # *add* methods to this class -- do *not* change given code and do not
-#   # override any methods
-#   # Note: This is the most difficult class.  In the sample solution,
-#   #  preprocess_prog is about 15 lines long and
-#   # intersectWithSegmentAsLineResult is about 40 lines long
-#   attr_reader :x1, :y1, :x2, :y2
-#   def initialize (x1,y1,x2,y2)
-#     @x1 = x1
-#     @y1 = y1
-#     @x2 = x2
-#     @y2 = y2
-#   end
-# end
+  def intersect other
+    other.intersectLine self
+  end
+  def intersectPoint p
+    p.intersectLine self
+  end
 
-# # Note: there is no need for getter methods for the non-value classes
+  def intersectLine line
+    if real_close(@m, line.m)
+      check_for_no_points(real_close(@b, line.b))
+    else
+      x = (line.b - @b) / (@m - line.m)
+      y = @m * x + @b
+      Point.new(x,y)
+    end
+  end
 
-# class Intersect < GeometryExpression
-#   # *add* methods to this class -- do *not* change given code and do not
-#   # override any methods
-#   def initialize(e1,e2)
-#     @e1 = e1
-#     @e2 = e2
-#   end
-# end
+  def intersectVerticalLine vline
+    Point.new(vline.x, @m * vline.x + @b)
+  end
 
-# class Let < GeometryExpression
-#   # *add* methods to this class -- do *not* change given code and do not
-#   # override any methods
-#   # Note: Look at Var to guide how you implement Let
-#   def initialize(s,e1,e2)
-#     @s = s
-#     @e1 = e1
-#     @e2 = e2
-#   end
-# end
+  def intersectWithSegmentAsLineResult seg
+    seg
+  end
+end
 
-# class Var < GeometryExpression
-#   # *add* methods to this class -- do *not* change given code and do not
-#   # override any methods
-#   def initialize s
-#     @s = s
-#   end
-#   def eval_prog env # remember: do not change this method
-#     pr = env.assoc @s
-#     raise "undefined variable" if pr.nil?
-#     pr[1]
-#   end
-# end
+class VerticalLine < GeometryValue
+  # *add* methods to this class -- do *not* change given code and do not
+  # override any methods
+  attr_reader :x
+  def initialize x
+    @x = x
+  end
 
-# class Shift < GeometryExpression
-#   # *add* methods to this class -- do *not* change given code and do not
-#   # override any methods
-#   def initialize(dx,dy,e)
-#     @dx = dx
-#     @dy = dy
-#     @e = e
-#   end
-# end
+  def shift(dx,dy)
+    self # shifting no-points is no-points
+  end
+  def intersect other
+    other.intersectNoPoints self # will be NoPoints but follow double-dispatch
+  end
+  def intersectPoint p
+    self # intersection with point and no-points is no-points
+  end
+  def intersectLine line
+    self # intersection with line and no-points is no-points
+  end
+  def intersectVerticalLine vline
+    self # intersection with line and no-points is no-points
+  end
+  # if self is the intersection of (1) some shape s and (2)
+  # the line containing seg, then we return the intersection of the
+  # shape s and the seg.  seg is an instance of LineSegment
+  def intersectWithSegmentAsLineResult seg
+    self
+  end
+end
+
+class LineSegment < GeometryValue
+  # *add* methods to this class -- do *not* change given code and do not
+  # override any methods
+  # Note: This is the most difficult class.  In the sample solution,
+  #  preprocess_prog is about 15 lines long and
+  # intersectWithSegmentAsLineResult is about 40 lines long
+  attr_reader :x1, :y1, :x2, :y2
+  def initialize (x1,y1,x2,y2)
+    @x1 = x1
+    @y1 = y1
+    @x2 = x2
+    @y2 = y2
+  end
+
+  def preprocess_prog
+    self # no pre-processing to do here
+  end
+
+  def shift(dx,dy)
+    self # shifting no-points is no-points
+  end
+  def intersect other
+    other.intersectNoPoints self # will be NoPoints but follow double-dispatch
+  end
+  def intersectPoint p
+    self # intersection with point and no-points is no-points
+  end
+  def intersectLine line
+    self # intersection with line and no-points is no-points
+  end
+  def intersectVerticalLine vline
+    self # intersection with line and no-points is no-points
+  end
+  # if self is the intersection of (1) some shape s and (2)
+  # the line containing seg, then we return the intersection of the
+  # shape s and the seg.  seg is an instance of LineSegment
+  def intersectWithSegmentAsLineResult seg
+    self
+  end
+end
+
+# Note: there is no need for getter methods for the non-value classes
+
+class Intersect < GeometryExpression
+  # *add* methods to this class -- do *not* change given code and do not
+  # override any methods
+  def initialize(e1,e2)
+    @e1 = e1
+    @e2 = e2
+  end
+  def preprocess_prog
+  end
+  def eval_prog env
+  end
+end
+
+class Let < GeometryExpression
+  # *add* methods to this class -- do *not* change given code and do not
+  # override any methods
+  # Note: Look at Var to guide how you implement Let
+  def initialize(s,e1,e2)
+    @s = s
+    @e1 = e1
+    @e2 = e2
+  end
+
+  def preprocess_prog
+  end
+  def eval_prog env
+  end
+end
+
+class Var < GeometryExpression
+  # *add* methods to this class -- do *not* change given code and do not
+  # override any methods
+  def initialize s
+    @s = s
+  end
+  def eval_prog env # remember: do not change this method
+    pr = env.assoc @s
+    raise "undefined variable" if pr.nil?
+    pr[1]
+  end
+
+  def preprocess_prog
+  end
+end
+
+class Shift < GeometryExpression
+  # *add* methods to this class -- do *not* change given code and do not
+  # override any methods
+  def initialize(dx,dy,e)
+    @dx = dx
+    @dy = dy
+    @e = e
+  end
+
+  def preprocess_prog
+  end
+  def eval_prog env
+  end
+end
--- week9/hw7/hw7testsprovided.rb
+++ week9/hw7/hw7testsprovided.rb
@@ -21,6 +21,7 @@ SEVEN = 7.0
 TEN = 10.0
 
 #Point Tests
+puts "Point:"
 a = Point.new(THREE,FIVE)
 if not (a.x == THREE and a.y == FIVE)
 	puts "Point is not initialized properly"
@@ -57,111 +58,161 @@ else
 end
 
 #Line Tests
-# b = Line.new(THREE,FIVE)
-# if not (b.m == THREE and b.b == FIVE)
-# 	puts "Line not initialized properly"
-# end
-# if not (b.eval_prog([]) == b)
-# 	puts "Line eval_prog should return self"
-# end
-# if not (b.preprocess_prog == b)
-# 	puts "Line preprocess_prog should return self"
-# end
+puts "Line:"
+b = Line.new(THREE,FIVE)
+if not (b.m == THREE and b.b == FIVE)
+	puts "Line not initialized properly"
+else
+	puts "GOOD"
+end
+if not (b.eval_prog([]) == b)
+	puts "Line eval_prog should return self"
+else
+	puts "GOOD"
+end
+if not (b.preprocess_prog == b)
+	puts "Line preprocess_prog should return self"
+else
+	puts "GOOD"
+end
 
-# b1 = b.shift(THREE,FIVE)
-# if not (b1.m == THREE and b1.b == ONE)
-# 	puts "Line shift not working properly"
-# end
+b1 = b.shift(THREE,FIVE)
+if not (b1.m == THREE and b1.b == ONE)
+	puts "Line shift not working properly"
+else
+	puts "GOOD"
+end
 
-# b2 = b.intersect(Line.new(THREE,FIVE))
-# if not (((b2.is_a? Line)) and b2.m == THREE and b2.b == FIVE)
-# 	puts "Line intersect not working properly"
-# end
-# b3 = b.intersect(Line.new(THREE,FOUR))
-# if not ((b3.is_a? NoPoints))
-# 	puts "Line intersect not working properly"
-# end
+b2 = b.intersect(Line.new(THREE,FIVE))
+if not (((b2.is_a? Line)) and b2.m == THREE and b2.b == FIVE)
+	puts "Line intersect not working properly"
+else
+	puts "GOOD"
+end
+b3 = b.intersect(Line.new(THREE,FOUR))
+if not ((b3.is_a? NoPoints))
+	puts "Line intersect not working properly"
+else
+	puts "GOOD"
+end
 
 # #VerticalLine Tests
+# puts "VerticalLine:"
 # c = VerticalLine.new(THREE)
 # if not (c.x == THREE)
 # 	puts "VerticalLine not initialized properly"
+# else
+#	puts "GOOD"
 # end
 
 # if not (c.eval_prog([]) == c)
 # 	puts "VerticalLine eval_prog should return self"
+# else
+#	puts "GOOD"
 # end
 # if not (c.preprocess_prog == c)
 # 	puts "VerticalLine preprocess_prog should return self"
+# else
+#	puts "GOOD"
 # end
 # c1 = c.shift(THREE,FIVE)
 # if not (c1.x == SIX)
 # 	puts "VerticalLine shift not working properly"
+# else
+#	puts "GOOD"
 # end
 # c2 = c.intersect(VerticalLine.new(THREE))
 # if not ((c2.is_a? VerticalLine) and c2.x == THREE )
 # 	puts "VerticalLine intersect not working properly"
+# else
+#	puts "GOOD"
 # end
 # c3 = c.intersect(VerticalLine.new(FOUR))
 # if not ((c3.is_a? NoPoints))
 # 	puts "VerticalLine intersect not working properly"
+# else
+#	puts "GOOD"
 # end
 
 # #LineSegment Tests
+# puts "LineSegment:"
 # d = LineSegment.new(ONE,TWO,-THREE,-FOUR)
 # if not (d.eval_prog([]) == d)
 # 	puts "LineSegement eval_prog should return self"
+# else
+#	puts "GOOD"
 # end
 # d1 = LineSegment.new(ONE,TWO,ONE,TWO)
 # d2 = d1.preprocess_prog
 # if not ((d2.is_a? Point)and d2.x == ONE and d2.y == TWO)
 # 	puts "LineSegment preprocess_prog should convert to a Point"
 # 	puts "if ends of segment are real_close"
+# else
+#	puts "GOOD"
 # end
 
 # d = d.preprocess_prog
 # if not (d.x1 == -THREE and d.y1 == -FOUR and d.x2 == ONE and d.y2 == TWO)
 # 	puts "LineSegment preprocess_prog should make x1 and y1"
 # 	puts "on the left of x2 and y2"
+# else
+#	puts "GOOD"
 # end
 
 # d3 = d.shift(THREE,FIVE)
 # if not (d3.x1 == ZERO and d3.y1 == ONE and d3.x2 == FOUR and d3.y2 == SEVEN)
 # 	puts "LineSegment shift not working properly"
+# else
+#	puts "GOOD"
 # end
 
 # d4 = d.intersect(LineSegment.new(-THREE,-FOUR,ONE,TWO))
 # if not (((d4.is_a? LineSegment)) and d4.x1 == -THREE and d4.y1 == -FOUR and d4.x2 == ONE and d4.y2 == TWO)
 # 	puts "LineSegment intersect not working properly"
+# else
+#	puts "GOOD"
 # end
 # d5 = d.intersect(LineSegment.new(TWO,THREE,FOUR,FIVE))
 # if not ((d5.is_a? NoPoints))
 # 	puts "LineSegment intersect not working properly"
+# else
+#	puts "GOOD"
 # end
 
 # #Intersect Tests
+# puts "Intersect:"
 # i = Intersect.new(LineSegment.new(-ONE,-TWO,THREE,FOUR), LineSegment.new(THREE,FOUR,-ONE,-TWO))
 # i1 = i.preprocess_prog.eval_prog([])
 # if not (i1.x1 == -ONE and i1.y1 == -TWO and i1.x2 == THREE and i1.y2 == FOUR)
 # 	puts "Intersect eval_prog should return the intersect between e1 and e2"
+# else
+#	puts "GOOD"
 # end
 
 # #Var Tests
+# puts "Var:"
 # v = Var.new("a")
 # v1 = v.eval_prog([["a", Point.new(THREE,FIVE)]])
 # if not ((v1.is_a? Point) and v1.x == THREE and v1.y == FIVE)
 # 	puts "Var eval_prog is not working properly"
+# else
+#	puts "GOOD"
 # end
 # if not (v.preprocess_prog == v)
 # 	puts "Var preprocess_prog should return self"
+# else
+#	puts "GOOD"
 # end
 
 # #Let Tests
+# puts "Let:"
 # l = Let.new("a", LineSegment.new(-ONE,-TWO,THREE,FOUR),
 #              Intersect.new(Var.new("a"),LineSegment.new(THREE,FOUR,-ONE,-TWO)))
 # l1 = l.preprocess_prog.eval_prog([])
 # if not (l1.x1 == -ONE and l1.y1 == -TWO and l1.x2 == THREE and l1.y2 == FOUR)
 # 	puts "Let eval_prog should evaluate e2 after adding [s, e1] to the environment"
+# else
+#	puts "GOOD"
 # end
 
 # #Let Variable Shadowing Test
@@ -170,12 +221,17 @@ end
 # l2 = l2.preprocess_prog.eval_prog([["a",Point.new(0,0)]])
 # if not (l2.x1 == -ONE and l2.y1 == -TWO and l2.x2 == THREE and l2.y2 == FOUR)
 # 	puts "Let eval_prog should evaluate e2 after adding [s, e1] to the environment"
+# else
+#	puts "GOOD"
 # end
 
 
 # #Shift Tests
+# puts "Shift:"
 # s = Shift.new(THREE,FIVE,LineSegment.new(-ONE,-TWO,THREE,FOUR))
 # s1 = s.preprocess_prog.eval_prog([])
 # if not (s1.x1 == TWO and s1.y1 == THREE and s1.x2 == SIX and s1.y2 == 9)
 # 	puts "Shift should shift e by dx and dy"
+# else
+#	puts "GOOD"
 # end
